c2640d50494e51447b93fd79cef7e14dd3e84bae
pastordev <pastordev@jarvis-toolkit.com>
2025-03-10 18:43:12 +0200

    Add homework.py

---
+++ homework.py
@@ -0,0 +1,10 @@
+"""
+Task:
+    Menu is a list of dishes ( length of list == 10, create list from 10 types of dishes):
+
+    1) Reverse order of dishes list
+    2) Print table between dishes and their numbers ( indexes )
+        EXAMPLE ( don't use explicit values, ex. prints with new strings )
+            BMW - 0
+            Mercedes - 1
+"""
