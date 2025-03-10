68fec12ee25175c674ad57c86b2e654c0d604380
pastordev <pastordev@jarvis-toolkit.com>
2025-03-10 17:36:06 +0200

    update data-types

---
+++ lecture.py
@@ -0,0 +1,27 @@
+# Topic: Data-types ( Structures )
+
+"""
+1) Untyped language. *Type - categorical group of objects with some responsibilities.
+2) Variables ( links to objects in memory ):
+    my_storage = object()
+3) Types of objects:
+    -------IMMUTABLE----------------
+    - integer, with floating decimal
+    - string or bytes strings
+4) Object
+    Everything in Python is object.
+    Object is a base thing. ( ex. "New hero without any characteristics" )
+"""
+
+# 2
+my_storage = object()
+
+# 3
+# Number
+number_integer: int = 10
+number_floating: float = 10.2
+
+# Strings
+regular_string: str = 'Vanya'
+bytes_string: bytes = b'Vanya'
+
---
+++ practise.py
@@ -0,0 +1,8 @@
+"""
+Task:
+1) Create the variables
+    1. Number
+    2. String
+2) Print second variable in upper-case ( Caps Lock )
+3) Print count of bits in first variable
+"""
