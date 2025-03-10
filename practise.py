07e8d502c10f3c7ea0cdc70e46c4d69e654acb40
Ivanchik228ar <ivanbondarets971@gmail.com>
2025-03-10 16:51:21 +0100

    держи я сделал много полезного

--- practise.py
+++ practise.py
@@ -6,3 +6,6 @@ Task:
 2) Print second variable in upper-case ( Caps Lock )
 3) Print count of bits in first variable
 """
+zadanienachalo: int = 1
+notnumber: str = "Ok"
+print (notnumber.isupper())
