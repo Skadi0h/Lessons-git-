de18b5b5a545986c5a8b690134c3ee7502032e52
Ivanchik228ar <ivanbondarets971@gmail.com>
2025-03-30 22:41:25 +0200

    calculate a factorial

--- challenges/loops/while/task7.py
+++ challenges/loops/while/task7.py
@@ -7,10 +7,15 @@ A palindrome is a number that reads the same forward and backward.
 For example: 121, 1331, 444 are palindromes.
 научится отделять цифры от любого числа с помощью операции деления с остатком(остаток от деления на 10, 100, 1000, 10000, 100000 ...)
 """
-user_number = int(input("Write please number:"))
-user_number_reverse = str(user_number)[::-1]
-# while user_number > 0:
-if str(user_number) == user_number_reverse:
-    print("Your number is palimdromes!")
-else:
-    print("Not this time!")
+# user_number = int(input("Write please number:"))
+# user_number_reverse = str(user_number)[::-1]
+# # while user_number > 0:
+# if str(user_number) == user_number_reverse:
+#     print("Your number is palimdromes!")
+# else:
+#     print("Not this time!")
+number = int(input("Write number: "))
+
+while number > 0:
+    print(number % 10)
+
--- challenges/loops/while/task8.py
+++ challenges/loops/while/task8.py
@@ -3,3 +3,11 @@ Task 8
 Calculate and print the factorial of a given number using a while loop.
 Example: 5! = 5 × 4 × 3 × 2 × 1 = 120
 """
+number = int(input("Write a number: "))
+i = 1
+
+while number > 1:
+    i *= number
+    number -= 1
+
+print( "Answer:", i)
