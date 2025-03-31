7f6c3f982441235fb9cba58defb55b6c93be2feb
Ivanchik228ar <ivanbondarets971@gmail.com>
2025-03-31 17:11:29 +0200

    calculater

--- challenges/loops/loops_test.py
+++ challenges/loops/loops_test.py
@@ -4,3 +4,24 @@ Simple calculator that performs +, -, *, / operations.
 User can repeatedly enter numbers and an operation.
 Enter 'q' as the operation to quit the program.
 """
+while True:
+    first_number = input("Write the first number:")
+    if first_number == "q":
+        break
+    operation = input("Write an operation:")
+    second_number = input("Write the second number:")
+    if operation == "+":
+        result = float(first_number) + float(second_number)
+
+    elif operation == "-":
+        result = float(first_number) - float(second_number)
+
+    elif operation == "*":
+        result = float(first_number) * float(second_number)
+
+    elif operation == "/":
+        result = float(first_number) / float(second_number)
+
+    else:
+        print()
+    print(result)
