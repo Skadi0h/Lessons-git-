753e5267cb334f0d03dd0c1ce990f5d9d3464d75
pastordev <pastordev@jarvis-toolkit.com>
2025-03-31 18:36:24 +0300

    upd

--- challenges/loops/loops_test.py
+++ challenges/loops/loops_test.py
@@ -4,24 +4,63 @@ Simple calculator that performs +, -, *, / operations.
 User can repeatedly enter numbers and an operation.
 Enter 'q' as the operation to quit the program.
 """
-while True:
-    first_number = input("Write the first number:")
-    if first_number == "q":
-        break
-    operation = input("Write an operation:")
-    second_number = input("Write the second number:")
-    if operation == "+":
-        result = float(first_number) + float(second_number)
+# while True:
+#     first_number = input("Write the first number:")
+#     if first_number == "q":
+#         break
+#     operation = input("Write an operation:")
+#     second_number = input("Write the second number:")
+#     if operation == "+":
+#         result = float(first_number) + float(second_number)
+#
+#     elif operation == "-":
+#         result = float(first_number) - float(second_number)
+#
+#     elif operation == "*":
+#         result = float(first_number) * float(second_number)
+#
+#     elif operation == "/":
+#         result = float(first_number) / float(second_number)
+#
+#     else:
+#         print()
+#     print(result)
 
-    elif operation == "-":
-        result = float(first_number) - float(second_number)
 
-    elif operation == "*":
-        result = float(first_number) * float(second_number)
+# TODO: 9/12 Done! cc. A
 
-    elif operation == "/":
-        result = float(first_number) / float(second_number)
 
-    else:
-        print()
-    print(result)
+# with Exceptions and map
+
+# operations_map = {
+#     '+': lambda x, y: x + y,
+#     '-': lambda x, y: x - y,
+#     '*': lambda x, y: x * y,
+#     '/': lambda x, y: x / y
+# }
+#
+# while True:
+#     first_number = input("Write the first number: ")
+#     if first_number == "q":
+#         break
+#     operation = input("Write an operation: ")
+#     second_number = input("Write the second number: ")
+#     try:
+#         if operation in operations_map:
+#             result = operations_map[operation](first_number, second_number)
+#         else:
+#             raise ValueError('Not supported operator')
+#     except ValueError as e: # print('error')
+#         print('Try again! Choose operator from: +, -, *, /')
+#         continue
+#     except ZeroDivisionError as e:
+#         print('Try again! You cannot divide by zero')
+#         continue
+#     print(result)
+
+
+# one-line calculator
+# loc = {}
+# while True:
+#     exec(input(), globals(), loc)
+#     print(loc['result'])
