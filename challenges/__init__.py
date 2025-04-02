c56186896176f8c38b49ac2acc2cd3ab04571503
Ivanchik228ar <ivanbondarets971@gmail.com>
2025-04-02 22:14:55 +0200

    timer

---
+++ challenges/__init__.py
@@ -0,0 +1,11 @@
+import time #fromAI
+seconds = int(input("Write, how long will be your timer: "))
+def timer(seconds):
+    while seconds > 0:
+        print(seconds, "seconds is left")
+        time.sleep(1) #fromAI
+        seconds -= 1
+    print("Time is over!")
+timer(seconds)
+
+
