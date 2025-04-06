ebd0df17ee3d849236d95234343c3b6efa7160c2
pastordev <pastordev@jarvis-toolkit.com>
2025-04-06 12:07:20 +0300

    upd

--- topics/loops/Creative_loops_task.py
+++ topics/loops/Creative_loops_task.py
@@ -1,1 +1,9 @@
-#программа которая будет решать мою рутину
+import time #fromAI
+seconds = int(input("Write, how long will be your timer: "))
+def timer(seconds):
+    while seconds > 0:
+        print(seconds, "seconds is left")
+        time.sleep(1) #fromAI
+        seconds -= 1
+    print("Time is over!")
+timer(seconds)
